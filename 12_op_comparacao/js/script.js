1972ff981acddb56199f482fe3dd74f1962e7798
Pedro Henrique <pedroauthentico@gmail.com>
2023-01-21 13:38:00 -0400

    Aula 13 finalizada

---
+++ 12_op_comparacao/js/script.js
@@ -0,0 +1,29 @@
+var idade = 16;
+var possuiCarro = 0;
+var nome = "Matheus";
+
+if(idade >= 18){
+    console.log("O usuário pode fazer a carteira");
+}
+
+if(idade <= 17){
+    console.log("O usuário não pode fazer a carteira");
+}
+if(possuiCarro){
+    console.log("O usuário já pode andar de carro");
+}
+
+if(nome =="Matheus"){
+    console.log("O nome é Matheus");
+}
+
+if(nome !="Matheus"){
+    console.log("O nome não é Matheus");
+}
+if(20 > 100){
+    console.log("Passou");
+}
+
+if(100 < 20){
+    console.log("Passou 2");
+}
