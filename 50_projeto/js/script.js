b0e362706c924a739a8aec6ac42048c341417002
PeHMB <93653905+PeHMB@users.noreply.github.com>
2023-02-12 16:10:22 -0400

    CSS e HTML finalizado

--- 50_projeto/js/script.js
+++ 50_projeto/js/script.js
@@ -1,1 +1,30 @@
-console.log("Testando");
+//função que adiciona tarefa
+function addTask(){
+    //titulo da tarefa
+    const taskTitle = document.querySelector("#task-title").value;
+
+    if(taskTitle){
+        //clona template
+        const template = document.querySelector(".template");
+
+        const newTask = template.cloneNode(true);
+
+        console.log(newTask);
+
+        //adiciona titulo
+        newTask.querySelector(".task-title")
+
+    }
+
+}
+
+
+//evento de adicionar tarefa
+const addBtn = document.querySelector("#add-btn");
+
+addBtn.addEventListener("click", function (e) {
+    
+    e.preventDefault();
+
+    addTask();
+});
