51821b0c1c557db33bf16b49da43b39d4ede9a0b
PeHMB <93653905+PeHMB@users.noreply.github.com>
2023-02-13 15:32:44 -0400

    addTask completo

--- 50_projeto/js/script.js
+++ 50_projeto/js/script.js
@@ -1,30 +1,48 @@
 //função que adiciona tarefa
-function addTask(){
+function addTask() {
     //titulo da tarefa
     const taskTitle = document.querySelector("#task-title").value;
 
-    if(taskTitle){
+    if (taskTitle) {
         //clona template
         const template = document.querySelector(".template");
 
         const newTask = template.cloneNode(true);
 
-        console.log(newTask);
-
         //adiciona titulo
-        newTask.querySelector(".task-title")
+        newTask.querySelector(".task-title").textContent = taskTitle;
 
+
+        //remover classes desnecessarias    
+        newTask.classList.remove("template");
+        newTask.classList.remove("hide");
+
+        //adiciona tarefa na lista
+        const list = document.querySelector("#task-list");
+
+        list.appendChild(newTask);
+
+        //adicionar evento de remover
+        const removeBtn = newTask.querySelector(".remove-btn").addEventListener("click", function () {
+            removeTask(this);
+        })
+        //limpar texto
+        document.querySelector("#task-title").value = "";
     }
-
 }
 
+//função de remover tarefa
+function removeTask(task) {
+    task.parentNode.remove(true);
+}
 
 //evento de adicionar tarefa
 const addBtn = document.querySelector("#add-btn");
 
 addBtn.addEventListener("click", function (e) {
-    
+
     e.preventDefault();
 
     addTask();
 });
+
