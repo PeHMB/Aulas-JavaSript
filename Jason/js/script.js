224885b3bf22df208f4bcf5858b4db7c409a39de
PeHMB <93653905+PeHMB@users.noreply.github.com>
2023-02-19 09:26:40 -0400

    Fim do Json

---
+++ Jason/js/script.js
@@ -0,0 +1,45 @@
+const objs = [
+    {
+        nome: "Matheus",
+        idade: 30,
+        esta_trabalhando: true,
+        detalhes_profissao: {
+            profissao: 'Programador',
+            empresa: 'Empresa X'
+        },
+        hobbies: [
+            'Programar', 'Correr', 'Ler'
+        ]
+    },
+    {
+        nome: "João",
+        idade: 25,
+        esta_trabalhando: false,
+        detalhes_profissao: {
+            profissao: null,
+            empresa: null
+        },
+        hobbies: [
+            'Jogar', 'Academia'
+        ]
+        
+    }
+]
+
+
+
+//JSON
+const jsonData = JSON.stringify(objs)
+
+console.log(jsonData);
+console.log(typeof jsonData);
+
+//converter json para objeto
+const objData = JSON.parse(jsonData);
+
+console.log(jsonData);
+console.log(typeof objData);
+
+objData.map((pessoa) => {
+    console.log(pessoa.nome);
+});
